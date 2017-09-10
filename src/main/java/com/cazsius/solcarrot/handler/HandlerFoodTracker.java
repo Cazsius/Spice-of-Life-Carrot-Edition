07154b361813f180a1573f03e3230b7505e57778
Cazsius <cazsius0518@gmail.com>
2017-09-09 23:07:17 -0400

    Formatted

--- src/main/java/com/cazsius/solcarrot/handler/HandlerFoodTracker.java
+++ src/main/java/com/cazsius/solcarrot/handler/HandlerFoodTracker.java
@@ -18,33 +18,30 @@ public class HandlerFoodTracker {
 		float storedHP = event.player.getMaxHealth();
 
 		MaxHealthHandler.updateFoodHPModifier(event.player);
-		if (storedHP < event.player.getMaxHealth()) 
-		{
+		if (storedHP < event.player.getMaxHealth()) {
 			event.player.playSound(SoundEvents.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
 			event.player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, event.player.posX, event.player.posY + 2,
 					event.player.posZ, event.player.motionX, event.player.motionY, event.player.motionZ, new int[0]);
-			if(event.player.world.isRemote) 
-			{
+			if (event.player.world.isRemote) {
 				int hpm = HandlerConfiguration.getHeartsPerMilestone();
-				TextComponentTranslation milestoneMessage = new TextComponentTranslation("What a unique flavor! You've gained " +(hpm==1?"a heart!":hpm + " hearts!"));
+				TextComponentTranslation milestoneMessage = new TextComponentTranslation(
+						"What a unique flavor! You've gained " + (hpm == 1 ? "a heart!" : hpm + " hearts!"));
 				event.player.sendMessage(milestoneMessage);
-				
+
 				int foodsEaten = food.getCount();
 				int milestone = 0;
 				int[] milestoneArray = HandlerConfiguration.getMilestoneArray();
-				while (milestone < milestoneArray.length && foodsEaten + 1 > milestoneArray[milestone]) 
-				{
+				while (milestone < milestoneArray.length && foodsEaten + 1 > milestoneArray[milestone]) {
 					milestone++;
 				}
-				
-				if (milestone == milestoneArray.length)
-				{
-					milestoneMessage = new TextComponentTranslation("Your desire to seek unique foods is finally satisfied.");
-				}
-				else
-				{
-					milestoneMessage = new TextComponentTranslation("Sample another "+(milestoneArray[milestone]-milestoneArray[milestone-1])+
-							" varieties of food to gain another "+(hpm==1?"heart!":hpm + " hearts!"));
+
+				if (milestone == milestoneArray.length) {
+					milestoneMessage = new TextComponentTranslation(
+							"Your desire to seek unique foods is finally satisfied.");
+				} else {
+					milestoneMessage = new TextComponentTranslation(
+							"Sample another " + (milestoneArray[milestone] - milestoneArray[milestone - 1])
+									+ " varieties of food to gain another " + (hpm == 1 ? "heart!" : hpm + " hearts!"));
 				}
 				event.player.sendMessage(milestoneMessage);
 			}
