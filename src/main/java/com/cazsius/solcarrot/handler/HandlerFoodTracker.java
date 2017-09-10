bab9ea739a808b35618178fb039d5ca6882ed49a
Cazsius <cazsius0518@gmail.com>
2017-09-09 23:09:07 -0400

    Ported changes from 1.11.2

--- src/main/java/com/cazsius/solcarrot/handler/HandlerFoodTracker.java
+++ src/main/java/com/cazsius/solcarrot/handler/HandlerFoodTracker.java
@@ -23,9 +23,27 @@ public class HandlerFoodTracker {
 			event.player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, event.player.posX, event.player.posY + 2,
 					event.player.posZ, event.player.motionX, event.player.motionY, event.player.motionZ, new int[0]);
 			if (event.player.world.isRemote) {
-				TextComponentTranslation milestone = new TextComponentTranslation("You've reached a new milestone! "
-						+ HandlerConfiguration.getHeartsPerMilestone() + " hearts have been added!");
-				event.player.sendMessage(milestone);
+				int hpm = HandlerConfiguration.getHeartsPerMilestone();
+				TextComponentTranslation milestoneMessage = new TextComponentTranslation(
+						"What a unique flavor! You've gained " + (hpm == 1 ? "a heart!" : hpm + " hearts!"));
+				event.player.sendMessage(milestoneMessage);
+
+				int foodsEaten = food.getCount();
+				int milestone = 0;
+				int[] milestoneArray = HandlerConfiguration.getMilestoneArray();
+				while (milestone < milestoneArray.length && foodsEaten + 1 > milestoneArray[milestone]) {
+					milestone++;
+				}
+
+				if (milestone == milestoneArray.length) {
+					milestoneMessage = new TextComponentTranslation(
+							"Your desire to seek unique foods is finally satisfied.");
+				} else {
+					milestoneMessage = new TextComponentTranslation(
+							"Sample another " + (milestoneArray[milestone] - milestoneArray[milestone - 1])
+									+ " varieties of food to gain another " + (hpm == 1 ? "heart!" : hpm + " hearts!"));
+				}
+				event.player.sendMessage(milestoneMessage);
 			}
 
 		}
