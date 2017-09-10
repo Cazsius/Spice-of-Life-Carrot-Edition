db6aa75650b5b0410e16fa5540abba1c7fb724cc
brennan <brennan@interplay-learning.com>
2017-09-09 20:12:40 -0600

    Guess who learned packet handling from a tutorial?

--- src/main/java/com/cazsius/solcarrot/common/CommonProxy.java
+++ src/main/java/com/cazsius/solcarrot/common/CommonProxy.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CommonProxy {
 
 	public void preInit(FMLPreInitializationEvent e) {
-		PacketHandler.registerMessages("modtut");
+		PacketHandler.registerMessages("solcarrot");
 	}
 	
 	
