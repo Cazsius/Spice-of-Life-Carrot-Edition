104839198ea1be3f3477298ad1c81d68a11d115d
Cazsius <cazsius0518@gmail.com>
2017-09-26 18:31:49 -0400

    Updated Version Number

--- src/main/java/com/cazsius/solcarrot/lib/Constants.java
+++ src/main/java/com/cazsius/solcarrot/lib/Constants.java
@@ -3,7 +3,7 @@ package com.cazsius.solcarrot.lib;
 public final class Constants {
 	public static final String MOD_ID = "solcarrot";
 	public static final String MOD_NAME = "Spice of Life: Carrot Edition";
-	public static final String VERSION_NUMBER = "1.1.0";
+	public static final String VERSION_NUMBER = "1.2.0";
 	public static final String CLIENT_PROXY_CLASS = "com.cazsius.solcarrot.client.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.cazsius.solcarrot.common.CommonProxy";
 
