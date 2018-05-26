e4347a0f4d8e2af4d2fc0f33ae44ba125d89a58a
juliand665 <julian.dunskus@gmail.com>
2018-05-26 21:58:12 +0200

    auto-formatting
    so my actual commits are less noisy

--- src/main/java/com/cazsius/solcarrot/capability/FoodCapability.java
+++ src/main/java/com/cazsius/solcarrot/capability/FoodCapability.java
@@ -16,80 +16,76 @@ import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 
 public class FoodCapability implements ICapabilitySerializable<NBTBase> {
-
+	
 	public Set<FoodInstance> foodList = new HashSet<>();
-
+	
 	public FoodCapability() {
 	}
-
+	
 	public void addFood(Item item, int meta) {
 		foodList.add(new FoodInstance(item, meta));
 	}
-
+	
 	@CapabilityInject(FoodCapability.class)
 	public static Capability<FoodCapability> FOOD_CAPABILITY = null;
-
+	
 	@Override
 	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
 		return capability == FOOD_CAPABILITY;
 	}
-
+	
 	@Override
 	@SuppressWarnings("unchecked")
 	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
 		return capability == FOOD_CAPABILITY ? (T) this : null;
 	}
-
+	
 	@Override
-	public NBTBase serializeNBT() 
-	{
+	public NBTBase serializeNBT() {
 		NBTTagList list = new NBTTagList();
-		for (FoodInstance fInstance : this.foodList) 
-		{
+		for (FoodInstance fInstance : this.foodList) {
 			ResourceLocation location = Item.REGISTRY.getNameForObject(fInstance.item());
-			if (location == null) continue;
+			if (location == null)
+				continue;
 			String toWrite = location.toString();
-			toWrite+="@"+fInstance.meta();
+			toWrite += "@" + fInstance.meta();
 			list.appendTag(new NBTTagString(toWrite));
 		}
 		return list;
 	}
-
+	
 	@Override
-	public void deserializeNBT(NBTBase nbt) 
-	{
+	public void deserializeNBT(NBTBase nbt) {
 		NBTTagList list = (NBTTagList) nbt;
-		for (int i = 0; i < list.tagCount(); i++)
-		{
+		for (int i = 0; i < list.tagCount(); i++) {
 			String toDecompose = ((NBTTagString) list.get(i)).getString();
 			int index = toDecompose.indexOf("@");
-			if (index < 0) continue;
+			if (index < 0)
+				continue;
 			String name = toDecompose.substring(0, index);
-			int meta = Integer.decode(toDecompose.substring(index + 1)); 
+			int meta = Integer.decode(toDecompose.substring(index + 1));
 			this.addFood(Item.getByNameOrId(name), meta);
 		}
 	}
-
+	
 	public int getCount() {
 		return foodList.size();
 	}
-
+	
 	public boolean hasEaten(Item foodJustEaten, int meta) {
 		return foodList.contains(new FoodInstance(foodJustEaten, meta));
 	}
-
-
+	
 	public void clearFood() {
 		foodList.clear();
 	}
-
+	
 	public void copyFoods(FoodCapability food) {
 		clearFood();
 		foodList.addAll(food.foodList);
 	}
-
-	public List<FoodInstance> getHistory() 
-	{
+	
+	public List<FoodInstance> getHistory() {
 		return new ArrayList<FoodInstance>(foodList);
 	}
 }
