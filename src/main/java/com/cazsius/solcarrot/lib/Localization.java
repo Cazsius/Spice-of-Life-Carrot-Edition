651ad7d76e27372c4c4155af4f790c4decb2c415
Julian Dunskus <julian.dunskus@gmail.com>
2019-02-26 00:28:40 +0100

    reorganizing localization

---
+++ src/main/java/com/cazsius/solcarrot/lib/Localization.java
@@ -0,0 +1,28 @@
+package com.cazsius.solcarrot.lib;
+
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+public class Localization {
+	/** e.g. keyString("tooltip", "eaten_status.not_eaten_1") -> "tooltip.solcarrot.eatenStatus.not_eaten_1") */
+	private static String keyString(String domain, String path) {
+		return domain + "." + Constants.MOD_ID + "." + path;
+	}
+	
+	public static String localized(String domain, IForgeRegistryEntry entry, String path, Object... params) {
+		final ResourceLocation location = entry.getRegistryName();
+		assert location != null;
+		return localized(domain, location.getResourcePath() + "." + path, params);
+	}
+	
+	public static String localized(String domain, String path, Object... params) {
+		return I18n.format(keyString(domain, path), params);
+	}
+	
+	public static String localizedQuantity(String domain, String path, int number) {
+		return number == 1
+				? I18n.format(keyString(domain, path + ".singular"))
+				: I18n.format(keyString(domain, path + ".plural"), number);
+	}
+}
