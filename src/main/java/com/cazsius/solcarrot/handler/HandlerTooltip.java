1c5a56bbbf25b0bcec32e74e9f08ecb738770bb8
Julian Dunskus <julian.dunskus@gmail.com>
2019-02-26 00:31:32 +0100

    cleaned up tooltip handling

--- src/main/java/com/cazsius/solcarrot/handler/HandlerTooltip.java
+++ src/main/java/com/cazsius/solcarrot/handler/HandlerTooltip.java
@@ -1,10 +1,11 @@
 package com.cazsius.solcarrot.handler;
 
+import com.cazsius.solcarrot.capability.FoodCapability;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,41 +13,42 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import squeek.applecore.api.AppleCoreAPI;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.cazsius.solcarrot.capability.FoodCapability;
+import static com.cazsius.solcarrot.lib.Localization.localized;
 
 @SideOnly(Side.CLIENT)
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(Side.CLIENT)
 public class HandlerTooltip {
 	
-	public static boolean isValidFood(ItemStack itemStack) {
+	private static boolean isValidFood(ItemStack itemStack) {
 		return AppleCoreAPI.accessor.isFood(itemStack);
 	}
 
 	@SubscribeEvent
 	public static void onItemTooltip(ItemTooltipEvent event) {
-		if (HandlerConfiguration.isFoodTooltipEnabled() && event.getEntityPlayer() != null
-				&& event.getItemStack() != null && isValidFood(event.getItemStack())) {
-			EntityPlayer player = event.getEntityPlayer();
-			Item foodJustEaten = event.getItemStack().getItem();
-			FoodCapability food = player.getCapability(FoodCapability.FOOD_CAPABILITY, null);
-			boolean hasBeenEaten = food.hasEaten(foodJustEaten, event.getItemStack().getMetadata());
-
-			List<String> toolTipStringsToAdd = new ArrayList<String>();
-
-			if (hasBeenEaten) {
-				toolTipStringsToAdd.add(TextFormatting.DARK_GRAY.toString() + TextFormatting.ITALIC
-						+ I18n.format("solcarrot.tooltip.hpeaten"));
-			} else {
-				toolTipStringsToAdd.add(TextFormatting.DARK_AQUA.toString() + TextFormatting.ITALIC
-						+ I18n.format("solcarrot.tooltip.noteaten1"));
-				toolTipStringsToAdd.add(TextFormatting.DARK_AQUA.toString() + TextFormatting.ITALIC
-						+ I18n.format("solcarrot.tooltip.noteaten2"));
-			}
-
-			event.getToolTip().addAll(toolTipStringsToAdd);
-
+		if (!HandlerConfiguration.isFoodTooltipEnabled()) return;
+		
+		if (event.getEntityPlayer() == null) return;
+		EntityPlayer player = event.getEntityPlayer();
+		
+		if (!isValidFood(event.getItemStack())) return;
+		Item foodJustEaten = event.getItemStack().getItem();
+		
+		FoodCapability food = player.getCapability(FoodCapability.FOOD_CAPABILITY, null);
+		assert food != null;
+		boolean hasBeenEaten = food.hasEaten(foodJustEaten, event.getItemStack().getMetadata());
+		
+		List<String> tooltip = event.getToolTip();
+		
+		if (hasBeenEaten) {
+			String formatting = "" + TextFormatting.DARK_GRAY + TextFormatting.ITALIC;
+			tooltip.add(formatting + localized("tooltip", "eaten"));
+		} else {
+			String formatting = "" + TextFormatting.DARK_AQUA + TextFormatting.ITALIC;
+			tooltip.add(formatting + localized("tooltip", "not_eaten_1"));
+			tooltip.add(formatting + localized("tooltip", "not_eaten_2"));
 		}
 	}
 }
