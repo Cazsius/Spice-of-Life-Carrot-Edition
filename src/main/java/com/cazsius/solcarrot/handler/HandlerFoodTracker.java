ba13e0cf36b29f6c76ad46aa7388acb8b6a33032
Julian Dunskus <julian.dunskus@gmail.com>
2019-02-26 00:32:44 +0100

    cleaned up tracking and max health modifying

--- src/main/java/com/cazsius/solcarrot/handler/HandlerFoodTracker.java
+++ src/main/java/com/cazsius/solcarrot/handler/HandlerFoodTracker.java
@@ -1,62 +1,55 @@
 package com.cazsius.solcarrot.handler;
 
 import com.cazsius.solcarrot.capability.FoodCapability;
-
+import com.cazsius.solcarrot.command.Command;
+import com.cazsius.solcarrot.lib.ProgressInfo;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import squeek.applecore.api.food.FoodEvent;
+
+import static com.cazsius.solcarrot.lib.Localization.localized;
+import static com.cazsius.solcarrot.lib.Localization.localizedQuantity;
 
 @Mod.EventBusSubscriber
 public class HandlerFoodTracker {
 	
 	@SubscribeEvent
 	public static void onFoodEaten(FoodEvent.FoodEaten event) {
-		FoodCapability food = event.player.getCapability(FoodCapability.FOOD_CAPABILITY, null);
-		food.addFood(event.food.getItem(), event.food.getMetadata());
+		EntityPlayer player = event.player;
 		
-		float storedHP = event.player.getMaxHealth();
+		FoodCapability foodCapability = FoodCapability.get(player);
+		foodCapability.addFood(event.food.getItem(), event.food.getMetadata());
 		
-		MaxHealthHandler.updateFoodHPModifier(event.player);
-		if (storedHP < event.player.getMaxHealth()) {
-			event.player.playSound(SoundEvents.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
-			event.player.world.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, event.player.posX, event.player.posY + 2,
-					event.player.posZ, event.player.motionX, event.player.motionY, event.player.motionZ, new int[0]);
-			if (event.player.world.isRemote) {
-				int hpm = HandlerConfiguration.getHeartsPerMilestone();
-				TextComponentTranslation milestoneMessage = new TextComponentTranslation("solcarrot.message.foodeaten",
-						(hpm == 1 ? I18n.translateToLocal("solcarrot.message.foodeaten.singleheart")
-								: hpm + " " + I18n.translateToLocal("solcarrot.message.foodeaten.multipleheart")));
-				TextComponentString milestoneMessageSend = new TextComponentString(TextFormatting.DARK_AQUA + milestoneMessage.getUnformattedText());
-				
-				event.player.sendStatusMessage(milestoneMessageSend, HandlerConfiguration.shouldShowProgressAboveHotbar());
-				
-				int foodsEaten = food.getCount();
-				int milestone = 0;
-				int[] milestoneArray = HandlerConfiguration.getMilestoneArray();
-				while (milestone < milestoneArray.length && foodsEaten + 1 > milestoneArray[milestone]) {
-					milestone++;
-				}
-				
-				if (milestone == milestoneArray.length) {
-					milestoneMessage = new TextComponentTranslation("solcarrot.message.desire.lost");
-					milestoneMessageSend = new TextComponentString(TextFormatting.DARK_AQUA + milestoneMessage.getUnformattedText());
-				} else {
-					milestoneMessage = new TextComponentTranslation("solcarrot.message.desire",
-							(milestoneArray[milestone] - milestoneArray[milestone - 1]),
-							(hpm == 1 ? I18n.translateToLocal("solcarrot.message.foodeaten.singleheart")
-									: hpm + " " + I18n.translateToLocal("solcarrot.message.foodeaten.multipleheart")));
-					milestoneMessageSend = new TextComponentString(TextFormatting.DARK_AQUA + milestoneMessage.getUnformattedText());
-				}
-				event.player.sendStatusMessage(milestoneMessageSend, HandlerConfiguration.shouldShowProgressAboveHotbar());
-			}
+		float storedHP = player.getMaxHealth();
+		
+		MaxHealthHandler.updateFoodHPModifier(player);
+		if (storedHP < player.getMaxHealth()) {
+			player.playSound(SoundEvents.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
+			player.world.spawnParticle(
+					EnumParticleTypes.VILLAGER_HAPPY,
+					player.posX, player.posY + 2, player.posZ,
+					player.motionX, player.motionY, player.motionZ
+			);
 			
+			if (player.world.isRemote) {
+				ProgressInfo progressInfo = ProgressInfo.getProgressInfo(player);
+				
+				String heartsDescription = localizedQuantity("message", "milestone_achieved.hearts", HandlerConfiguration.getHeartsPerMilestone());
+				
+				String milestoneAchievedMessage = localized("message", "milestone_achieved", heartsDescription);
+				
+				String nextMilestoneMessage;
+				if (progressInfo.hasReachedMax()) {
+					nextMilestoneMessage = localized("message", "desire.finished");
+				} else {
+					nextMilestoneMessage = localized("message", "desire.continues", progressInfo.foodsUntilNextMilestone(), heartsDescription);
+				}
+				
+				Command.showMessage(player, milestoneAchievedMessage + "\n" + nextMilestoneMessage);
+			}
 		}
 	}
-	
 }
--- src/main/java/com/cazsius/solcarrot/handler/MaxHealthHandler.java
+++ src/main/java/com/cazsius/solcarrot/handler/MaxHealthHandler.java
@@ -5,86 +5,72 @@
  ******************************************************************************/
 package com.cazsius.solcarrot.handler;
 
-import java.util.UUID;
-
-import com.cazsius.solcarrot.capability.FoodCapability;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
-
+import com.cazsius.solcarrot.lib.ProgressInfo;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
+import java.util.UUID;
+
 @Mod.EventBusSubscriber
 public class MaxHealthHandler {
-	public static final UUID MILESTONE_HEALTH_MODIFIER_ID = UUID.fromString("b20d3436-0d39-4868-96ab-d0a4856e68c6");
-
+	private static final UUID MILESTONE_HEALTH_MODIFIER_ID = UUID.fromString("b20d3436-0d39-4868-96ab-d0a4856e68c6");
+	
 	@SubscribeEvent
 	public static void onPlayerLogin(PlayerLoggedInEvent event) {
 		EntityPlayer player = event.player;
-		World world = player.world;
-
-		if (!world.isRemote) {
+		
+		if (!player.world.isRemote) {
 			updateFoodHPModifier(player);
 		}
 	}
-
+	
 	@SubscribeEvent
 	public static void onPlayerClone(PlayerEvent.Clone event) {
-		IAttributeInstance oldMaxHealthInstance = event.getOriginal().getAttributeMap()
-				.getAttributeInstance(SharedMonsterAttributes.MAX_HEALTH);
-		AttributeModifier modifier = oldMaxHealthInstance.getModifier(MILESTONE_HEALTH_MODIFIER_ID);
-		Multimap<String, AttributeModifier> multimap = HashMultimap.<String, AttributeModifier>create();
-		multimap.put(SharedMonsterAttributes.MAX_HEALTH.getName(), modifier);
-		event.getEntityPlayer().getAttributeMap().applyAttributeModifiers(multimap);
+		updateHealthModifier(event.getEntityPlayer(), getHealthModifier(event.getOriginal()));
 	}
-
+	
 	public static void updateFoodHPModifier(EntityPlayer player) {
-		IAttributeInstance maxHealthInstance = player.getAttributeMap()
-				.getAttributeInstance(SharedMonsterAttributes.MAX_HEALTH);
-		AttributeModifier modifier = maxHealthInstance.getModifier(MILESTONE_HEALTH_MODIFIER_ID);
-
-		int startingHealth = getStartingHearts();
-		double healthPenalty = -20 + startingHealth * 2;
-
-		double addedHealthFromFood = getFoodMilestoneHearts(player) * 2 * HandlerConfiguration.getHeartsPerMilestone();
-		double overallHealthModifier = healthPenalty + addedHealthFromFood;
-
-		if (modifier == null || modifier.getAmount() != overallHealthModifier) {
-			Multimap<String, AttributeModifier> multimap = HashMultimap.<String, AttributeModifier>create();
-			modifier = new AttributeModifier(MILESTONE_HEALTH_MODIFIER_ID, "Food Milestone Health Mod",
-					overallHealthModifier, 0);
-			multimap.put(SharedMonsterAttributes.MAX_HEALTH.getName(), modifier);
-			player.getAttributeMap().applyAttributeModifiers(multimap);
+		AttributeModifier prevModifier = getHealthModifier(player);
+		
+		int healthPenalty = 2 * (HandlerConfiguration.getDefaultHeartCount() - 10);
+		
+		int milestonesAchieved = ProgressInfo.getProgressInfo(player).milestonesAchieved;
+		int addedHealthFromFood = milestonesAchieved * 2 * HandlerConfiguration.getHeartsPerMilestone();
+		
+		double totalHealthModifier = healthPenalty + addedHealthFromFood;
+		
+		if (prevModifier == null || prevModifier.getAmount() != totalHealthModifier) {
+			AttributeModifier modifier = new AttributeModifier(
+					MILESTONE_HEALTH_MODIFIER_ID,
+					"Health Gained from Trying New Foods",
+					totalHealthModifier,
+					0
+			);
+			updateHealthModifier(player, modifier);
 			// This should never happen, but let's be defensive.
 			if (player.getHealth() > player.getMaxHealth()) {
 				player.setHealth(player.getMaxHealth());
 			}
 		}
 	}
-
-	public static int getMaxHearts() {
-		return HandlerConfiguration.getDefaultHeartCount() + HandlerConfiguration.getNumMilestones();
+	
+	private static AttributeModifier getHealthModifier(EntityPlayer player) {
+		return maxHealthAttribute(player).getModifier(MILESTONE_HEALTH_MODIFIER_ID);
 	}
-
-	public static int getStartingHearts() {
-		return HandlerConfiguration.getDefaultHeartCount();
+	
+	private static void updateHealthModifier(EntityPlayer player, AttributeModifier modifier) {
+		IAttributeInstance attribute = maxHealthAttribute(player);
+		attribute.removeModifier(modifier);
+		attribute.applyModifier(modifier);
 	}
-
-	public static int getFoodMilestoneHearts(EntityPlayer player) {
-		FoodCapability food = player.getCapability(FoodCapability.FOOD_CAPABILITY, null);
-		int foodsEaten = food.getCount();
-		int milestones = 0;
-		int[] milestoneArray = HandlerConfiguration.getMilestoneArray();
-		while (milestones < milestoneArray.length && milestoneArray[milestones] <= foodsEaten) {
-			milestones++;
-		}
-		return milestones;
+	
+	private static IAttributeInstance maxHealthAttribute(EntityPlayer player) {
+		return player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
 	}
 }
