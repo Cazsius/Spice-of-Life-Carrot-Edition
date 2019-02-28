3d27ab22c59d7d8e07e71743e6a6f4c53fba4539
Julian Dunskus <julian.dunskus@gmail.com>
2019-02-28 19:24:07 +0100

    switched back to old config names
    these new names are not worth the migration effort required of users

--- src/main/java/com/cazsius/solcarrot/SOLCarrotConfig.java
+++ src/main/java/com/cazsius/solcarrot/SOLCarrotConfig.java
@@ -11,28 +11,36 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+// new config names + versioning commented out for now
+// TODO switch to new names when updating to next mc version
+
 @Mod.EventBusSubscriber
-@Config(modid = Constants.MOD_ID, name = Constants.MOD_ID + "-" + SOLCarrotConfig.version)
+@Config(modid = Constants.MOD_ID)//, name = Constants.MOD_ID + "-" + SOLCarrotConfig.version)
 public class SOLCarrotConfig {
-	static final String version = "1.5"; // only change this if the new version is not backwards-compatible with the old one.
+	//static final String version = "1.5"; // only change this if the new version is not backwards-compatible with the old one.
 	
-	@Config.Name("Base Hearts")
+	//@Config.Name("Base Hearts")
+	@Config.Name("defaultHeartCount")
 	@Config.Comment("Number of hearts you start out with.")
 	public static int baseHearts = 10;
 	
-	@Config.Name("Hearts per Milestone")
+	//@Config.Name("Hearts per Milestone")
+	@Config.Name("heartsPerMilestone")
 	@Config.Comment("Number of hearts you gain for reaching a new milestone.")
 	public static int heartsPerMilestone = 2;
 	
-	@Config.Name("Milestones")
+	//@Config.Name("Milestones")
+	@Config.Name("Milestone amounts")
 	@Config.Comment("A list of numbers of unique foods you need to eat to unlock each milestone, in ascending order.")
 	public static int[] milestones = {5, 10, 15, 20, 25};
 	
-	@Config.Name("Enable Food Status Tooltip")
+	//@Config.Name("Enable Food Status Tooltip")
+	@Config.Name("isFoodTooltipEnabled")
 	@Config.Comment("If true, foods indicate in their tooltips whether or not they have been eaten.")
 	public static boolean isFoodTooltipEnabled = true;
 	
-	@Config.Name("Show Progress Above Hotbar")
+	//@Config.Name("Show Progress Above Hotbar")
+	@Config.Name("shouldShowProgressAboveHotbar")
 	@Config.Comment("Whether the messages notifying you of reaching new milestones should be displayed above the hotbar or in chat.")
 	public static boolean shouldShowProgressAboveHotbar = true;
 	
