9dc9372045fbb2e62c3d008d1403c0acbdbb440e
Julian Dunskus <julian.dunskus@gmail.com>
2019-03-13 23:33:52 +0100

    auto-formatting

--- src/main/java/com/cazsius/solcarrot/SOLCarrotConfig.java
+++ src/main/java/com/cazsius/solcarrot/SOLCarrotConfig.java
@@ -3,7 +3,6 @@ package com.cazsius.solcarrot;
 import com.cazsius.solcarrot.capability.FoodCapability;
 import com.cazsius.solcarrot.handler.CapabilityHandler;
 import com.cazsius.solcarrot.handler.MaxHealthHandler;
-import com.cazsius.solcarrot.lib.Localization;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.management.PlayerList;
 import net.minecraftforge.common.config.Config;
