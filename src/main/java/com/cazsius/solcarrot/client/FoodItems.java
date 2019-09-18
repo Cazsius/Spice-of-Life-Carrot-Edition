aad45ec34b862d57d65d36f344f967797790bffd
Julian Dunskus <julian.dunskus@gmail.com>
2019-09-18 04:51:07 +0200

    fixed config update not affecting FoodItems
    …i had forgotten `@SubscribeEvent`

--- src/main/java/com/cazsius/solcarrot/client/FoodItems.java
+++ src/main/java/com/cazsius/solcarrot/client/FoodItems.java
@@ -7,8 +7,8 @@ import net.minecraft.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
 
 @OnlyIn(Dist.CLIENT)
-@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = SOLCarrot.MOD_ID)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = SOLCarrot.MOD_ID, bus = MOD)
 public final class FoodItems {
 	private static List<Item> foodsBeforeBlacklist;
 	private static List<Item> foods;
@@ -33,24 +33,19 @@ public final class FoodItems {
 		return new ArrayList<>(foods);
 	}
 	
-	@Mod.EventBusSubscriber(modid = SOLCarrot.MOD_ID, bus = MOD)
-	private static final class Setup {
-		@SubscribeEvent
-		public static void setUp(FMLCommonSetupEvent event) {
-			foodsBeforeBlacklist = ForgeRegistries.ITEMS.getValues().stream()
-				.filter(Item::isFood)
-				// sort by name
-				.sorted(Comparator.comparing(food -> I18n.format(food.getTranslationKey() + ".name")))
-				.collect(Collectors.toList());
-			
-			applyBlacklist();
-		}
+	@SubscribeEvent
+	public static void setUp(FMLCommonSetupEvent event) {
+		foodsBeforeBlacklist = ForgeRegistries.ITEMS.getValues().stream()
+			.filter(Item::isFood)
+			// sort by name
+			.sorted(Comparator.comparing(food -> I18n.format(food.getTranslationKey() + ".name")))
+			.collect(Collectors.toList());
+		
+		applyBlacklist();
 	}
 	
 	@SubscribeEvent
-	public static void onPostConfigChanged(ConfigChangedEvent.PostConfigChangedEvent event) {
-		if (!event.getModID().equals(SOLCarrot.MOD_ID)) return;
-		
+	public static void onConfigUpdate(ModConfig.ModConfigEvent event) {
 		applyBlacklist();
 	}
 	
@@ -59,6 +54,4 @@ public final class FoodItems {
 			.filter(SOLCarrotConfig::isAllowed)
 			.collect(Collectors.toList());
 	}
-	
-	private FoodItems() {}
 }
